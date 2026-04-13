cd359d6cb69618f011a3d30d60eeb932aeaad643
Kiara Zapana Murillo <kz2023077087@virtual.upt.pe>
2026-04-13 19:02:39 +0000

    Corregir aserción en la prueba de Fibonacci para n=10

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -73,7 +73,7 @@ def test_get_nth_fibonacci_ten():
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
 
 
 def test_get_nth_fibonacci_negative_raises_value_error():
